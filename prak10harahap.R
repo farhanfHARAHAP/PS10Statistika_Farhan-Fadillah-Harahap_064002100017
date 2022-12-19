c8876125ade8a35c11d1802ef89f296de56fcf80
farhanfHARAHAP <91046795+farhanfHARAHAP@users.noreply.github.com>
2022-12-19 12:25:17 +0700

    Add files via upload

---
+++ prak10harahap.R
@@ -0,0 +1,30 @@
+#Latihan 1
+1-pbinom(3,15,0.1)
+
+pbinom(1,15,prob=0.1)
+
+1-pbinom(6,15,prob=0.9)
+1-pbinom(5,15,prob=0.9)
+
+dbinom(10, 15, prob=0.9)
+
+dbinom(15, 15, prob=0.9)
+
+#Latihan 2
+dbinom(4, size=12, prob=0.2)
+
+dbinom(0, size=12, prob=0.2) + dbinom(1, size=12, prob=0.2) + dbinom(2, size=12,
+                                                                     prob=0.2) + dbinom(3, size=12, prob=0.2) + dbinom(4, size=12, prob=0.2)
+pbinom(4, size=12, prob=0.2)
+
+#Latihan 3
+dbinom(5, size=20, prob=(1-0.15))
+
+dbinom(3, size=20, prob=0.15)
+
+dbinom(8, size=20, prob=(1-0.15))
+
+dbinom(2, size=20, prob=0.15)
+
+
+
